03558f69e75547a1e4d5af3c5a3e704912cef4ed
SOHAIB EL MEDIOUNI <sohaib.elmediouni23@gmail.com>
2021-05-23 22:27:36 +0100

    first commit

---
+++ src/main/java/com/livraison/demo/GestionDeLivraisonApplication.java
@@ -0,0 +1,13 @@
+package com.livraison.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeLivraisonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeLivraisonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/livraison/demo/GestionDeLivraisonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livraison.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeLivraisonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
